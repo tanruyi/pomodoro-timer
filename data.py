200561b9d5970bd43bb620547eb03412cdfd4a01
ruyi <tan_ruyi@hotmail.com>
2023-06-07 15:33:47 +0800

    - background image added

--- data.py
+++ data.py
@@ -1,22 +1,22 @@
 backgroundData = [
     {
         'name': 'dried flowers', 
-        'fileLocation': '',
+        'fileLocation': '/static/assets/dried-flowers.jpg',
         'attribute': '<a href="https://www.freepik.com/free-photo/dried-flower-window-shadow-floral-image_11460360.htm#query=aesthetic%20background&position=49&from_view=search&track=robertav1_2_sidr">Image by rawpixel.com</a> on Freepik',
     },
     {
         'name': 'rainbow sky',
-        'fileLocation': '',
+        'fileLocation': '/static/assets/rainbow-sky.jpg',
         'attribute': '<a href="https://www.freepik.com/free-vector/aesthetic-pastel-pink-background-rainbow-sky-with-glitter-design-vector_20346136.htm#page=3&query=aesthetic%20background&position=21&from_view=search&track=robertav1_2_sidr">Image by rawpixel.com</a> on Freepik',
     },
     {
         'name': 'snowy rooftops',
-        'fileLocation': '', 
+        'fileLocation': '/static/assets/snowy-rooftops.jpg', 
         'attribute': 'Photo by <a href="https://unsplash.com/@5tep5?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Aleksandr Popov</a> on <a href="https://unsplash.com/wallpapers/cool/aesthetic?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>',
     },
     {
         'name': 'snowy streets',
-        'fileLocation': '',
+        'fileLocation': '/static/assets/snowy-streets.jpg',
         'attribute': 'https://www.artstation.com/artwork/D5Vlxo',
     }
 ]
--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
+
+from data import backgroundData
 
 # create the app
 app = Flask(__name__)
         
+indexOfBackgroundImage = 0;
+
+@app.route("/")
+def home():
+    return render_template("base.html", imageURL = backgroundData[indexOfBackgroundImage]['fileLocation'])
+    
 # we only run the app if this file is run, the app should not run if this file is imported by another file
 if __name__ == '__main__':
     # setting debug as True will trigger auto reruns of the server when the code changes, to remove in production
-    app.run(debug=True)
-
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+    app.run(port=8000, debug=True)
